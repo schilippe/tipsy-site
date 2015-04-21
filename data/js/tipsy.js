3a094a85926339eb4b6eb2f92cc25430209ec90f
Philippe Schiff <schilippe@gmail.com>
2015-04-20 22:36:33 -0400

    added link tage generator. tipst.txt is next

--- data/js/tipsy.js
+++ data/js/tipsy.js
@@ -3,6 +3,63 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
+//handle link tage generation
+$(function() {
+
+    $('#linkTagGenerateBtn').on('click', (function(e) {
+    
+        $('#placeFollowing').empty();
+        $('#generatedLinkTag').empty();
+    
+        var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+        var name = $('#linkTagName').val();
+        var paypalEmail = $('#payPalEmail').val();
+        var dwollaKey = $('#dwollaKey').val();
+
+        var data = ""
+        var nameIn = ""
+        if (name != "") {
+            nameIn += " name='" + name + "'";
+        }
+
+        if (paypalEmail != "") {
+            if (!(regex.test(paypalEmail))) {
+                $('#noData').html("<div class='alert alert-warning text-center' id='alertMessage'>Make sure your paypal email is valid.</div>");
+                window.setTimeout(function() {
+                    $("#alertMessage").fadeTo(500, 0).slideUp(500, function() {
+                        $(this).remove();
+                    });
+                }, 2500);
+                return
+            }
+
+            data += " data-paypal='" + paypalEmail + "'";
+        }
+
+        if (dwollaKey != "") {
+            data += " data-dwolla='" + dwollaKey + "'";
+        }
+        var textArea = "<code> &ltlink rel='author'" + nameIn + data + "&gt</code>";
+
+        if (data != "") {
+            $('#noData').empty();
+            $('#placeFollowing').html("<p>Place the following in the <samp>HEAD</samp> of your site:</p>");
+            $('#generatedLinkTag').html(textArea);
+        } else {
+        
+            $('#noData').html("<div class='alert alert-warning text-center' id='alertMessage'>Please make sure you provided at least either PayPal or dwolla information.</div>");
+
+            window.setTimeout(function() {
+                $("#alertMessage").fadeTo(500, 0).slideUp(500, function() {
+                    $(this).remove();
+                });
+            }, 2500);
+
+        }
+    }));
+
+});
+
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -18,7 +75,7 @@ $(function() {
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
     }).on("blur", ".floating-label-form-group", function() {
@@ -36,17 +93,16 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-  $(document).ready(function(){
-    $('.modal').on('show.bs.modal', function () {
-      if ($(document).height() > $(window).height()) {
-        // no-scroll
-        //$('body').addClass("modal-open-noscroll");
-      }
-      else { 
-        //$('body').removeClass("modal-open-noscroll");
-      }
+$(document).ready(function() {
+    $('.modal').on('show.bs.modal', function() {
+        if ($(document).height() > $(window).height()) {
+            // no-scroll
+            //$('body').addClass("modal-open-noscroll");
+        } else {
+            //$('body').removeClass("modal-open-noscroll");
+        }
     })
-    $('.modal').on('hide.bs.modal', function () {
-       // $('body').removeClass("modal-open-noscroll");
+    $('.modal').on('hide.bs.modal', function() {
+        // $('body').removeClass("modal-open-noscroll");
     })
-  })
+})
